bb0eeae3790086b85a3156e6bdbd31fbfdd8c5e0
Drew Showalter <maelstroswim@gmail.com>
2018-12-07 15:00:13 +1100

    :wrench: upgrade and simplify underlying ruleset & deps.

--- index.js
+++ index.js
@@ -2,81 +2,78 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    mocha: true
+    mocha: true,
   },
   plugins: ['mocha', 'prettier'],
-  extends: ['eslint-config-airbnb', 'prettier'],
+  extends: ['airbnb', 'prettier'],
   rules: {
-    // http://eslint.org/docs/rules/no-use-before-define
-    'no-use-before-define': [
-      2,
-      {
-        functions: false
-      }
-    ],
-
     // http://eslint.org/docs/rules/quotes
     quotes: [
       2,
       'single',
       {
         avoidEscape: true,
-        allowTemplateLiterals: true
-      }
+        allowTemplateLiterals: true,
+      },
     ],
 
     // http://eslint.org/docs/rules/comma-dangle
-    'comma-dangle': [2, 'never'],
-
-    // http://eslint.org/docs/rules/no-shadow
-    'no-shadow': [0],
-
-    // http://eslint.org/docs/rules/no-param-reassign
-    'no-param-reassign': [2, { props: false }],
+    'comma-dangle': [2, 'always-multiline'],
 
     // http://eslint.org/docs/rules/strict
     strict: [0],
 
     'mocha/no-exclusive-tests': 'error',
 
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
-    'react/prefer-stateless-function': [1],
-
-    'react/require-extension': 'off',
-
     'generator-star-spacing': [
       'error',
       {
         before: true,
-        after: false
-      }
+        after: false,
+      },
     ],
 
-    'jsx-a11y/href-no-hash': 'off',
-
+    // http://eslint.org/docs/rules/no-shadow
+    // http://eslint.org/docs/rules/no-param-reassign
+    // http://eslint.org/docs/rules/no-use-before-define
+    'no-use-before-define': [
+      2,
+      {
+        functions: false,
+      },
+    ],
+    'no-param-reassign': [2, { props: false }],
+    'no-unused-expressions': 'off',
+    'no-mixed-operators': 'off',
+    'no-confusing-arrow': 'off',
+    'no-nested-ternary': 'off',
     'no-await-in-loop': 'off',
     'no-multi-assign': 'off',
-    'arrow-parens': 'off',
+    'no-else-return': 'off',
     'no-plusplus': 'off',
+    'no-shadow': [0],
+
+    'arrow-parens': 'off',
     'func-names': 0,
 
     'prettier/prettier': [
       'error',
       {
+        trailingComma: 'es5',
         singleQuote: true,
-        printWidth: 100
-      }
+        printWidth: 100,
+      },
     ],
     'max-len': [
       'error',
       {
         code: 100,
-        comments: 80,
+        comments: 100,
         ignoreUrls: true,
         ignoreStrings: true,
         ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true
-      }
-    ]
-  }
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
 };
